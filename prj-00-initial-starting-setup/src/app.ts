14f19f67fbd0509f9ea1a6eb722c87f3d72b01a6
kouheifujii <kohei.fujii@i-nations.tokyo>
2022-01-01 15:11:14 +0900

    initialize event handler.

--- prj-00-initial-starting-setup/src/app.ts
+++ prj-00-initial-starting-setup/src/app.ts
@@ -2,6 +2,10 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  inputTitleElement: HTMLInputElement;
+  inputDescriptionElement: HTMLInputElement;
+  inputPeapleElement: HTMLInputElement;
+
   constructor() {
     this.templateElement = document.getElementById(
       "project-input"
@@ -11,9 +15,31 @@ class ProjectInput {
     // 別ファイルの要素をコピーする。true で深いコピー
     const importNode = document.importNode(this.templateElement.content, true);
     this.element = importNode.firstElementChild as HTMLFormElement;
+    // 要素に id を付与する例。あえてやっている
+    this.element.id = "user-input";
+    // それぞれの input の値を取得
+    this.inputTitleElement = this.element.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.inputDescriptionElement = this.element.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.inputPeapleElement = this.element.querySelector(
+      "#peaple"
+    )! as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.inputTitleElement.value);
+  }
+  private configure() {
+    // bind で this を固定
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
