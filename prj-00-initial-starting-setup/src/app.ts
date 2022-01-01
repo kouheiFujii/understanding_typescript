3091c980699825623b46b65c93562104eeb492a2
kouheifujii <kohei.fujii@i-nations.tokyo>
2022-01-01 12:02:33 +0900

    create ProjectInput class.

--- prj-00-initial-starting-setup/src/app.ts
+++ prj-00-initial-starting-setup/src/app.ts
@@ -1,1 +1,22 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    ) as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app") as HTMLDivElement;
+    // このクラスをインスタンス化したときに即座に form をレンダリングさせたいため以下の処理を行っている
+    // 別ファイルの要素をコピーする。true で深いコピー
+    const importNode = document.importNode(this.templateElement.content, true);
+    this.element = importNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
