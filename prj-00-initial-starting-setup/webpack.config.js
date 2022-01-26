0bdc5f9c068416f577eaee0e4f8d56fb2816d2cb
kouheifujii <kohei.fujii@i-nations.tokyo>
2022-01-26 23:13:16 +0900

    Adding support with the ts-loader package

--- prj-00-initial-starting-setup/webpack.config.js
+++ prj-00-initial-starting-setup/webpack.config.js
@@ -2,9 +2,23 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/app.ts", // Entry root file.
+  // output 先の path と filename を指定している
   output: {
     // filename: "bundle.[contenthash].js", // contenthash is create a unique hash for every build.
     filename: "bundle.js", // output file name.
     path: path.resolve(__dirname, "dist"), // 絶対パスを指定
+  },
+  devtool: "inline-source-map", // tsconfig.json の sourceMap を使用するように支持
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"], // import するときに拡張子をスキップできるようになる
   },
 };
