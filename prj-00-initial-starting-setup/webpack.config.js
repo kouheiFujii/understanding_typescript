7e59a392873247c542db8a79a2f2ec6b1d2dc70b
kouheifujii <kohei.fujii@i-nations.tokyo>
2022-01-27 22:47:50 +0900

    add publicPath and mode.

--- prj-00-initial-starting-setup/webpack.config.js
+++ prj-00-initial-starting-setup/webpack.config.js
@@ -1,12 +1,14 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.ts", // Entry root file.
   // output 先の path と filename を指定している
   output: {
     // filename: "bundle.[contenthash].js", // contenthash is create a unique hash for every build.
     filename: "bundle.js", // output file name.
     path: path.resolve(__dirname, "dist"), // 絶対パスを指定
+    publicPath: "dist",
   },
   devtool: "inline-source-map", // tsconfig.json の sourceMap を使用するように支持
   module: {
